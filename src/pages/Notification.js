1a22c38f6e1651679991d91183ccf7c69145de8e
Abiodun <abiodunhodonu@gmail.com>
2023-04-29 17:23:58 +0100

    completed the notification page

--- src/pages/Notification.js
+++ src/pages/Notification.js
@@ -2,6 +2,8 @@ import './page styles/notification.css'
 import Main from '../layouts/Main'
 import '../components/home/home.css'
 import icon from '../images/david.jpeg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBell } from '@fortawesome/free-solid-svg-icons'
 
 
 
@@ -22,15 +24,158 @@ const Notification = () => {
         </div>
 
         <div className="notif-1">
-          bell image
-          <img src={icon} alt='notif' />
-          <img src={icon} alt='notif' />
-          <img src={icon} alt='notif' />
-          <img src={icon} alt='notif' />
-          <img src={icon} alt='notif' />
+          <div className="notif-box">
+          <FontAwesomeIcon className='notif-icon' icon={faBell} />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+          </div>
+          <p>New Tweet notifications for </p>
         </div>
-        <div className="notif-1"></div>
-        <div className="notif-1"></div>
+
+        <div className="notif-1">
+          <div className="notif-box">
+            <FontAwesomeIcon className='notif-icon' icon={faBell} />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+          </div>
+          <p>New Tweet notifications for </p>
+        </div>
+
+        <div className="notif-1">
+          <div className="notif-box">
+            <FontAwesomeIcon className='notif-icon' icon={faBell} />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+          </div>
+          <p>new notifications</p>
+        </div>
+
+        <div className="notif-1">
+          <div className='notif-box'>
+            <FontAwesomeIcon className='notif-icon' icon={faBell} />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+          </div>
+          <p>Subscribe to Jack Forge and get access to their exclusive content</p>
+        </div>
+
+        <div className="notif-1">
+          <div className="notif-box">
+            <FontAwesomeIcon className='notif-icon' icon={faBell} />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+          </div>
+          <p>Kemi and 17 others followed you </p>
+        </div>
+
+        <div className="notif-1">
+          <div className="notif-box">
+            <FontAwesomeIcon className='notif-icon' icon={faBell} />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+          </div>
+          <p>Bimpe Mentioned you </p>
+        </div>
+
+        <div className="notif-1">
+          <div className="notif-box">
+            <FontAwesomeIcon className='notif-icon' icon={faBell} />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+          </div>
+          <p>new notifications</p>
+        </div>
+
+        <div className="notif-1">
+          <div className="notif-box">
+            <FontAwesomeIcon className='notif-icon' icon={faBell} />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+          </div>
+          <p>new notifications</p>
+        </div>
+
+        <div className="notif-1">
+          <div className="notif-box">
+            <FontAwesomeIcon className='notif-icon' icon={faBell} />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+          </div>
+          <p>new notifications</p>
+        </div>
+
+        <div className="notif-1">
+          <div className="notif-box">
+            <FontAwesomeIcon className='notif-icon' icon={faBell} />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+          </div>
+          <p>new notifications</p>
+        </div>
+
+        <div className="notif-1">
+          <div className="notif-box">
+            <FontAwesomeIcon className='notif-icon' icon={faBell} />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+          </div>
+          <p>new notifications</p>
+        </div>
+
+        <div className="notif-1">
+          <div className="notif-box">
+            <FontAwesomeIcon className='notif-icon' icon={faBell} />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+          </div>
+          <p>new notifications</p>
+        </div>
+
+        <div className="notif-1">
+          <div className="notif-box">
+            <FontAwesomeIcon className='notif-icon' icon={faBell} />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+            <img src={icon} alt='notif' />
+          </div>
+          <p>new notifications</p>
+        </div>
       
       </div>
 
