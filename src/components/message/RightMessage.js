11dbd1b27cc8e1f79ff3a559eb79cdc7e203c0eb
Abiodun <abiodunhodonu@gmail.com>
2023-05-04 07:36:25 +0100

    completed message section

--- src/components/message/RightMessage.js
+++ src/components/message/RightMessage.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import './rightmessage.css'
 
 const RightMessage = () => {
   return (
-    <div>
-      <h1>Selecet Message</h1>
-      <h5>Choose from your existing conversations, start a new one, or just keep swimming.</h5>
-      <button>New message</button>
+    <div className='right-cover'>
+
+    <div className='right-container'>
+      <div className="right-content">
+        <h1>Selecet a message</h1>
+        <h5>Choose from your existing conversations, start a new one, or just keep swimming.</h5>
+        <button>New message</button>
+      </div>
+    </div>
     </div>
   )
 }
